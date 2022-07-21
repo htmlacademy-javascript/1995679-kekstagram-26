23ac2c21d731d4da815027dc963432eb35a8c15a
Alexey Demyanov <deal110988@him-mail.ch>
2022-07-21 21:49:56 +0200

    Выполнено задание 12.11

--- js/get-previews.js
+++ js/get-previews.js
@@ -1,5 +1,17 @@
+import { shuffle } from './util.js';
+
+const RANDOM_PHOTOS_AMOUNT = 10;
+
 const pictureTemplate = document.querySelector('#picture').content;
 const picturesPreviewsElement = document.querySelector('.pictures');
+const imageFiltersControls = document.querySelector('.img-filters');
+const defaultFilterButton = imageFiltersControls.querySelector('#filter-default');
+const randomFilterButton = imageFiltersControls.querySelector('#filter-random');
+const discussedFilterButton = imageFiltersControls.querySelector('#filter-discussed');
+
+const showImageFilters = () => {
+  imageFiltersControls.classList.remove('img-filters--inactive');
+};
 
 const getPhotoData = (photoData) => {
   const photo = {
@@ -22,23 +34,79 @@ const getPhotos = (data) => {
   return photos;
 };
 
-const getPreviews = (data) => {
-  const photos = getPhotos(data);
+const defaultFilter = (elements) => elements;
+
+const randomFilter = (elements) => {
+  shuffle(elements);
+  while (elements.length > RANDOM_PHOTOS_AMOUNT) {
+    elements.pop();
+  }
+};
+
+const discussedFilter = (elements) => {
+
+  const comparePopularity = (photoA, photoB) => {
+    const commentsA = photoA.comments.length;
+    const commentsB = photoB.comments.length;
+    return commentsB - commentsA;
+  };
+
+  elements.sort(comparePopularity);
+};
+
+const getPreviews = (data, filter) => {
+  const photos = getPhotos(data).slice();
 
   const photosFragment = document.createDocumentFragment();
 
-  photos.forEach((photo) => {
-    const pictureElement = pictureTemplate.cloneNode(true);
-    const url = pictureElement.querySelector('img');
-    const likes = pictureElement.querySelector('.picture__likes');
-    const comments = pictureElement.querySelector('.picture__comments');
-    url.src = photo.url;
-    likes.textContent = photo.likes;
-    comments.textContent = photo.comments.length;
-    photosFragment.appendChild(pictureElement);
-  });
+  if (filter) {filter(photos);}
+
+  photos
+    .forEach((photo) => {
+      const pictureElement = pictureTemplate.cloneNode(true);
+      const url = pictureElement.querySelector('img');
+      const likes = pictureElement.querySelector('.picture__likes');
+      const comments = pictureElement.querySelector('.picture__comments');
+      url.src = photo.url;
+      likes.textContent = photo.likes;
+      comments.textContent = photo.comments.length;
+      photosFragment.appendChild(pictureElement);
+    });
 
   picturesPreviewsElement.appendChild(photosFragment);
 };
 
-export { getPreviews };
+const changeActiveFilterButton = (button) => {
+  const allFilterButtons = document.querySelectorAll('.img-filters__button');
+  allFilterButtons.forEach((buttonElement) => {buttonElement.classList.remove('img-filters__button--active');});
+  button.classList.add('img-filters__button--active');
+};
+
+const setDefaultFilterButtonClick = (data) => {
+  defaultFilterButton.addEventListener('click', () => {
+    const allPreviews = document.querySelectorAll('.picture');
+    allPreviews.forEach((preview) => {preview.remove();});
+    getPreviews(data, defaultFilter);
+    changeActiveFilterButton(defaultFilterButton);
+  });
+};
+
+const setRandomFilterButtonClick = (data) => {
+  randomFilterButton.addEventListener('click', () => {
+    const allPreviews = document.querySelectorAll('.picture');
+    allPreviews.forEach((preview) => {preview.remove();});
+    getPreviews(data, randomFilter);
+    changeActiveFilterButton(randomFilterButton);
+  });
+};
+
+const setDiscussedFilterButtonClick = (data) => {
+  discussedFilterButton.addEventListener('click', () => {
+    const allPreviews = document.querySelectorAll('.picture');
+    allPreviews.forEach((preview) => {preview.remove();});
+    getPreviews(data, discussedFilter);
+    changeActiveFilterButton(discussedFilterButton);
+  });
+};
+
+export { getPreviews, showImageFilters, setDefaultFilterButtonClick, setRandomFilterButtonClick, setDiscussedFilterButtonClick };
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import { getPreviews } from './get-previews.js';
+import { getPreviews, showImageFilters, setDefaultFilterButtonClick, setRandomFilterButtonClick, setDiscussedFilterButtonClick } from './get-previews.js';
 import { imageUploadFormControlHandler } from './image-upload-form.js';
 import { changeImageScale } from './image-scale-control.js';
 import { createSlider, changeImageFilter } from './image-filter-control.js';
@@ -15,6 +15,12 @@ createSlider(0, 1, 'lower');
 changeImageFilter();
 
 getData(
-  (data) => {getPreviews(data);},
+  (data) => {
+    getPreviews(data);
+    showImageFilters();
+    setDefaultFilterButtonClick(data);
+    setRandomFilterButtonClick(data);
+    setDiscussedFilterButtonClick(data);
+  },
   (error) => {showAlert(`${error} - Не удается подгрузить данные. попробуйте еще`);}
 );
